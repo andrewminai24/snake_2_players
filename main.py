7b227dc5b353fdb386c1429ed6908567661f8cc6
Enkhmanlai Ganzorig <manlaig@users.noreply.github.com>
2017-10-26 16:30:12 -0700

    Changing the directions of the snakes

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ def main():
     numberOfPlayers = -1
     direction1 ='RIGHT'
 
+    foodPosition = [random.randrange(0, 72) * 10, random.randrange(0, 48) * 10]
+
     while numberOfPlayers == -1:
         screen.fill(white)
 
@@ -51,6 +53,38 @@ def main():
             direction2 = 'LEFT'
 
         pygame.display.flip()
+
+
+    while snake1.inBounds() or snake2.inBounds():
+        if not snake1.inBounds():
+            # gameOver("Player 2 wins!")
+            continue        # change
+
+        if not snake2.inBounds():
+            # gameOver("Player 1 wins!")
+            continue
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT and direction2 != 'RIGHT':
+                    direction2 = 'LEFT'
+                elif event.key == pygame.K_RIGHT and direction2 != 'LEFT':
+                    direction2 = 'RIGHT'
+                elif event.key == pygame.K_UP and direction2 != 'DOWN':
+                    direction2 = 'UP'
+                elif event.key == pygame.K_DOWN and direction2 != 'UP':
+                    direction2 = 'DOWN'
+                elif event.key == pygame.K_a and direction1 != 'RIGHT':
+                    direction1 = 'LEFT'
+                elif event.key == pygame.K_d and direction1 != 'LEFT':
+                    direction1 = 'RIGHT'
+                elif event.key == pygame.K_s and direction1 != 'UP':
+                    direction1 = 'DOWN'
+                elif event.key == pygame.K_w and direction1 != 'DOWN':
+                    direction1 = 'UP'
 
 
 
--- snake.py
+++ snake.py
@@ -3,3 +3,9 @@ class Snake:
     def __init__(self, positionX, positionY, bodyX, bodyY):
         self.position = [positionX, positionY]
         self.body = [[self.position[0], self.position[1]], [bodyX, bodyY], [bodyX - 10, bodyY - 10]]
+
+    def inBounds(self):
+        if self.position[0] < 0 or self.position[1] > 720 or self.position[1] < 0 or self.position[1] > 480:
+            return True
+
+        return False
